e5c3c8ebcb5e2a3962d10c0448ad59d2ebde71e1
nitrocon <77290402+nitrocon@users.noreply.github.com>
2024-12-31 17:25:39 +0100

    Add files via upload

---
+++ opencv/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Arbeitszimmer/Projekte/Python-Apps/opencv/include
+# Build directory: C:/Arbeitszimmer/Projekte/Python-Apps/opencv/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
